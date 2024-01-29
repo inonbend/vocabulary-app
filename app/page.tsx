b3eecdb55ec4e96abcc0670e2906c29589100c99
inon <inon.bendavid@pvml.com>
2024-01-29 11:59:38 +0200

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -70,15 +70,7 @@ export default function Home() {
                             <a
                                 className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
                             >
-                                By{' '}
-                                <Image
-                                    src="/logo.svg"
-                                    alt="Vercel Logo"
-                                    className="dark:invert"
-                                    width={100}
-                                    height={24}
-                                    priority
-                                />
+                                By Inon
                             </a>
                         </div>
                     </div>
